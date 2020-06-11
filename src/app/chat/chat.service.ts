e0ac47798a1b61cc8046fd94b5d60ded2d548067
MMAP <mehdi.patel@gmail.com>
2020-06-12 02:14:08 +0530

    added memes

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -15,6 +15,14 @@ export class ChatService {
   readonly token = environment.dialogflow.angularBot;
   readonly client = new ApiAiClient({accessToken: this.token});
   conversation = new BehaviorSubject<Message[]>([]); // default value as []
+  imgUrls: string[] = [
+    'https://qph.fs.quoracdn.net/main-qimg-1331730fa7e57497badfb0a6240d0c77.webp',
+    'https://static0.srcdn.com/wordpress/wp-content/uploads/2019/08/big-bang-theory-meme-sheldons-spot.jpg?q=50&fit=crop&w=740&h=740',
+    'https://preview.redd.it/qev9xsufs0g41.jpg?width=500&format=pjpg&auto=webp&s=3c3da5eb111bbb4b42a4245ee98578f32a96dfc7',
+    'https://i.pinimg.com/originals/d6/31/19/d63119a8e54dc3dba91ecb252ff83ad9.jpg',
+    'https://i.imgflip.com/2bohxz.jpg'
+  ];
+  imgIndex: number = 0;
 
 
   talk(){
@@ -50,6 +58,16 @@ export class ChatService {
         const botMessage = new Message(`It is ${temp}°C outside.`, 'bot');
         this.update(botMessage);
       });
+    }
+    index = msg.indexOf("meme");
+    if(index !== -1){
+      const imgMessage = new Message(this.imgUrls[this.imgIndex],'bot');
+      if(this.imgIndex <= 3){
+        this.imgIndex +=1;
+      }else{
+        this.imgIndex = 0;
+      }
+      return this.update(imgMessage);
     }
     return this.client.textRequest(msg)
     .then(res => {
